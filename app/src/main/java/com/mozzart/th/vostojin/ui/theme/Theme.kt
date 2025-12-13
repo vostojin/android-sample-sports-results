006f9b2d3a3c29700bc568bd44a8744e01b97a13
vostojin <vostojin@gmail.com>
2025-12-13 23:53:08 +0100

    Rename `MozzartTHVostojinTheme` to `MozzartTheme` for simplicity.

--- app/src/main/java/com/mozzart/th/vostojin/ui/theme/Theme.kt
+++ app/src/main/java/com/mozzart/th/vostojin/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.mozzart.th.vostojin.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun MozzartTHVostojinTheme(
+fun MozzartTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
