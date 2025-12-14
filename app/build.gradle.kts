e43babbe78b044c85a2da361fbfe05b77c6cd0fb
vostojin <vostojin@gmail.com>
2025-12-14 05:17:14 +0100

    Fix gradle build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,13 +25,13 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-    //compileOptions {
-    //    sourceCompatibility = JavaVersion.VERSION_11
-    //    targetCompatibility = JavaVersion.VERSION_11
-    //}
-    //kotlinOptions {
-    //    jvmTarget = "11"
-    //}
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
     buildFeatures {
         compose = true
     }
